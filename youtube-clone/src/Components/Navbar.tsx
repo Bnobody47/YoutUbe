290086e3ed97a9341d37423a090341882b079050
Bnobody47 <bnobodygetmoney@gmail.com>
2024-10-16 21:16:38 +0300

    Navbar for the youtube clone

--- youtube-clone/src/Components/Navbar.tsx
+++ youtube-clone/src/Components/Navbar.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { RxHamburgerMenu } from "react-icons/rx";
+import { FaYoutube } from "react-icons/fa";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='w-full bg-[#0c0c0c]'>
+      <div className="flex h-14 w-[95%] mx-auto">
+        <div className="flex items-center gap-8">
+          <RxHamburgerMenu className="text-xl"/>
+          <div className="flex items-center gap-1">
+            <FaYoutube className="text-3xl text-red-600"/>
+            <span className="text-xl">Youtube</span>
+          </div>
+        </div>
+        <div className=""></div>
+      </div>
+    </div>
   )
 }
 
