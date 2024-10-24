c09e0a22195fd743dac5253d9242d045d3d916f3
Bnobody47 <bnobodygetmoney@gmail.com>
2024-10-24 19:43:55 +0300

    changes to the search field

--- youtube-clone/src/Components/Navbar.tsx
+++ youtube-clone/src/Components/Navbar.tsx
@@ -1,10 +1,13 @@
 import { RxHamburgerMenu } from "react-icons/rx";
 import { FaYoutube } from "react-icons/fa";
+import { CiSearch } from "react-icons/ci";
+import { AiOutlineClose } from "react-icons/ai";
+
 
 function Navbar() {
   return (
     <div className='w-full bg-[#0c0c0c]'>
-      <div className="flex h-14 w-[95%] mx-auto">
+      <div className="flex justify-between h-14 w-[95%] mx-auto">
         <div className="flex items-center gap-8">
           <RxHamburgerMenu className="text-xl"/>
           <div className="flex items-center gap-1">
@@ -12,7 +15,28 @@ function Navbar() {
             <span className="text-xl">Youtube</span>
           </div>
         </div>
-        <div className=""></div>
+        <div className="flex items-center">
+          <form>
+            <div className="flex item-center sm:h-10 h-9 border-[0.6px] border-neutral-700 rounded-full overflow-hidden">
+              <div className="flex items-center pr-5">
+                <input 
+                  type="text"
+                  placeholder="Search" 
+                  className="w-96 px-3 text-lg text-zinc-300 placeholder-neutral-500 bg-[#0c0c0c] focus:outline-none"
+                
+                />
+                <AiOutlineClose className="text-lg cursor-pointer text-neutral-400"/>
+
+              </div>
+              <button className="w-16 flex items-center justify-center border-l-[1px] border-neutral-700">
+                <CiSearch className="text-2xl text-neutral-200"/>
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="">
+          {/*empty*/}
+        </div>
       </div>
     </div>
   )
